b3a46c75af82c06faaa8377166b5f551a193b605
Gant <GantMan@gmail.com>
2015-03-21 16:48:40 -0500

    added comments to example code

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -12,24 +12,38 @@ class MainController < UIViewController
     rmq.append(UIView, :alert_controller_section).tap do |acs|
       acs.append(UILabel, :alert_controller_title)
 
+      # Simple alert example that has an OK button (RedAlert default),
+      # doesn't care when it's pressed.
       acs.append(UIButton, :alert_controller_button).on(:tap) do
         rmq.alert("Minimal Alert")
       end
 
+      # Alert example that has an OK button (RedAlert default),
+      # puts a message when pressed.
       acs.append(UIButton, :alert_controller_two).on(:tap) do
         rmq.alert("Alert with Block") {
           puts "Alert with Block worked!"
         }
       end
 
+      # Alert example with changed title and message.
+      # OK button that doesn't care when pressed.
       acs.append(UIButton, :alert_controller_three).on(:tap) do
         rmq.alert(title: "New TITLE!", message: "So easy!")
       end
 
+      # Simple action sheet example.
+      # OK button that doesn't care when pressed.
       acs.append(UIButton, :alert_controller_four).on(:tap) do
         rmq.alert(title: "Hey there!", message: "My style is :sheet", style: :sheet)
       end
 
+      ##############################
+      #  Multiple button examples
+      ##############################
+
+
+      # Action sheet with template
       acs.append(UIButton, :alert_controller_five).on(:tap) do
         rmq.alert(message: "Would you like a sandwich?", actions: :yes_no_cancel, style: :sheet) { |action_type|
           case action_type
@@ -41,6 +55,7 @@ class MainController < UIViewController
         }
       end
 
+      # Alert example with 4 buttons, each made with `make_button` helper.
       acs.append(UIButton, :alert_controller_six).on(:tap) do
         ok = rmq.make_button {
           p "OK pressed"
@@ -63,6 +78,7 @@ class MainController < UIViewController
         rmq.alert(title: "Actions!", message: "Actions created with `make_button` helper.", actions: button_list)
       end
 
+      # Example of loading the actions array with native UIAlertAction objects.
       acs.append(UIButton, :alert_controller_advanced_button).on(:tap) do
 
         ok = UIAlertAction.actionWithTitle("OK", style: UIAlertActionStyleDefault, handler: -> (action) {
