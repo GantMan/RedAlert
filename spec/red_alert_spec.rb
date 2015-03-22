c6cb5802e0fc2afca7c0d6935774bf66ae7bc27c
Gant <GantMan@gmail.com>
2015-03-22 16:42:41 -0500

    testing :yes_no_cancel template

--- spec/red_alert_spec.rb
+++ spec/red_alert_spec.rb
@@ -53,8 +53,33 @@ describe 'RedAlert' do
 
       # Check for the buttons
       wait 1 do
-        @yes_no_controller.rmq(text: "Yes").size.should == 1
-        @yes_no_controller.rmq(text: "No").size.should == 1
+        @yes_no_cancel_controller.rmq(text: "Yes").size.should == 1
+        @yes_no_cancel_controller.rmq(text: "No").size.should == 1
+      end
+    end
+
+  end
+
+  describe "features of the :yes_no_cancel template" do
+
+    before do
+      @yes_no_cancel_controller = rmq.alert(message: 'yes no cancel', actions: :yes_no_cancel, show_now: false)
+      wait 1 do
+        rmq.view_controller.presentViewController(@yes_no_cancel_controller, animated: false, completion: nil)
+      end
+    end
+
+    it 'has the correct values for a :yes_no_cancel template' do
+      # Default title
+      @yes_no_cancel_controller.rmq(UILabel)[0].get.text.should == "Alert!"
+      # Assigned message
+      @yes_no_cancel_controller.rmq(UILabel)[1].get.text.should == 'yes no cancel'
+
+      # Check for the buttons
+      wait 1 do
+        @yes_no_cancel_controller.rmq(text: "Yes").size.should == 1
+        @yes_no_cancel_controller.rmq(text: "No").size.should == 1
+        @yes_no_cancel_controller.rmq(text: "Cancel").size.should == 1
       end
     end
 
