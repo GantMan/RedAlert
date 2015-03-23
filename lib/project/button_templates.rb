6cb83767861a29fa73e0f8e958c3b425ec8e9bd2
Gant <GantMan@gmail.com>
2015-03-22 19:05:53 -0500

    begin move to rmq.app.alert

--- lib/project/button_templates.rb
+++ lib/project/button_templates.rb
@@ -1,25 +1,25 @@
 module RubyMotionQuery
-  module ButtonTemplates
+  class App
 
     # When adding a new template do the following:
     # 1 - Add it here
     # 2 - Add the test
     # 3 - Add symbol to the README.md list
-    def add_template_actions(uiac, template, &block)
+    def self.add_template_actions(uiac, template, &block)
       case template
       when :yes_no
-        uiac.addAction(rmq.make_button("Yes", &block))
-        uiac.addAction(rmq.make_button("No", &block))
+        uiac.addAction(rmq.app.make_button("Yes", &block))
+        uiac.addAction(rmq.app.make_button("No", &block))
       when :yes_no_cancel
-        uiac.addAction(rmq.make_button("Yes", &block))
-        uiac.addAction(rmq.make_button("No", &block))
-        uiac.addAction(rmq.make_button({title: "Cancel", style: :cancel}, &block))
+        uiac.addAction(rmq.app.make_button("Yes", &block))
+        uiac.addAction(rmq.app.make_button("No", &block))
+        uiac.addAction(rmq.app.make_button({title: "Cancel", style: :cancel}, &block))
       when :ok_cancel
-        uiac.addAction(rmq.make_button("OK", &block))
-        uiac.addAction(rmq.make_button({title: "Cancel", style: :cancel}, &block))
+        uiac.addAction(rmq.app.make_button("OK", &block))
+        uiac.addAction(rmq.app.make_button({title: "Cancel", style: :cancel}, &block))
       when :delete_cancel
-        uiac.addAction(rmq.make_button({title: "Delete", style: :destructive}, &block))
-        uiac.addAction(rmq.make_button({title: "Cancel", style: :cancel}, &block))
+        uiac.addAction(rmq.app.make_button({title: "Delete", style: :destructive}, &block))
+        uiac.addAction(rmq.app.make_button({title: "Cancel", style: :cancel}, &block))
       end
     end
 
